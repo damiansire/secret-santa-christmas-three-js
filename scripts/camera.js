5728ea7b9ac3d5bf42437d2c8e626c7e6003e444
Damian Sire <damiansire@hotmail.com>
2022-12-11 17:51:55 -0300

    Agrego cambios de camara

---
+++ scripts/camera.js
@@ -0,0 +1,17 @@
+function ponerCamaraDeFrente(camera) {
+  camera.position.z = 4;
+  camera.position.y = 1;
+  camera.position.x = 2;
+  camera.rotation.x = 0;
+  camera.rotation.z = 0;
+  camera.rotation.y = 0;
+}
+
+function ponerCamaraDeArriba(camera) {
+  camera.position.z = 4;
+  camera.position.y = 5;
+  camera.position.x = 2;
+  camera.rotation.x = -1;
+  camera.rotation.z = 0;
+  camera.rotation.y = 0;
+}
